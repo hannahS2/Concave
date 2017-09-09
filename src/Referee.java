18a8845bd178adda9593ef6d5174a98a3c84bea4
chanheepark <ch13p@naver.com>
2017-09-09 22:10:54 +0900

    Merge branch 'master' of https://github.com/hannahS2/Concave
    # Conflicts:
    #	.idea/workspace.xml

--- src/Referee.java
+++ src/Referee.java
@@ -28,12 +28,15 @@ public class Referee {
         int posX = newStone.getX();
         int posY = newStone.getY();
         int color = newStone.getColor();
+        System.out.println("놓인 돌 색 : "+ color);
+
 
         //가로 앞
         int preRow = 0;
         int currentX = posX-1;
         int currentY = posY;
         while(currentX>=0 && stones[currentX][currentY]==color){
+            System.out.println("확인 중인 돌("+ posX+", "+posY+") 색 : "+stones[currentX][currentY]);
             preRow++;
             currentX--;
         }
@@ -42,7 +45,7 @@ public class Referee {
         currentX=posX+1;
         currentY=posY;
         while(currentX<=0 && stones[currentX][currentY]==color){
-            preRow++;
+            postRow++;
             currentX++;
         }
         if(preRow+postRow>=4){
@@ -50,20 +53,21 @@ public class Referee {
             return true;
         }
 
-        //새로 앞
+        //세로
+        //세로 앞
         int preCol = 0;
         currentX = posX;
         currentY = posY-1;
         while(currentY>=0 && stones[currentX][currentY]==color){
-            preRow++;
+            preCol++;
             currentY--;
         }
-        //새로 뒤
+        //세로 뒤
         int postCol = 0;
         currentX=posX;
         currentY=posY+1;
         while(currentY<=0 && stones[currentX][currentY]==color){
-            preRow++;
+            postCol++;
             currentY++;
         }
         if(preCol+postCol>=4){
@@ -71,8 +75,26 @@ public class Referee {
             return true;
         }
 
+
+        //대각선 1
         //대각선 좌상
+        int preDiagonal = 0;
+        currentX=posX-1;
+        currentY=posY-1;
+        while(currentX<=0 && currentY<=0 && stones[currentX][currentY]==color){
+            preDiagonal++;
+            currentX--;
+            currentY--;
+        }
+
         //대각선 우하
+        int postDiagonal = 0;
+
+
+        if(preDiagonal+postDiagonal>=4){
+            System.out.println(color+" 승리!");
+            return true;
+        }
 
 
 
